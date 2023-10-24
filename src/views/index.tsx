f0a97ba6c7065b1860362492a61eaaaab48cd909
Pratama89 <pratama.adiciptawan@gmail.com>
2023-10-24 16:23:48 +0700

    database firebase ubah data dari client2

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -5,16 +5,24 @@ import Image from "next/image"
 
 const ProductView = ({ products }: { products: ProductType[] }) => {
     return (
-        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 border-2 border-red-600'>
+        // style.product
+    <div className='w-full p-0.5 container'> 
+        {/* product_title */}
         <h1 className="text-2xl font-bold mt-5 text-center">Product</h1>
-        <div className="flex justify-center text-center mt-5">
+        {/* product_content */}
+        <div className="flex mt-5">
             {products.map((product: ProductType) => (
-                <div key={product.id} className="flex flex-col  ">
+                // product_content_item
+                <div key={product.id} className="w-1/4 p-3">
+                    {/* product_content_item_image */}
                     <div className="">
                         <img src={product.image} alt={product.name} width="300" height="300" />
                     </div>
+                    {/* product_content_item_name */}
                     <h4 className="mt-5 font-bold text-2xl">{product.name}</h4>
+                    {/* product_content_item_price */}
                     <p className="">Harga: {product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</p>
+                    {/* product_content_item_stock */}
                     <p className="">Stok: {product.stock}</p>
                 </div>
             ))}
