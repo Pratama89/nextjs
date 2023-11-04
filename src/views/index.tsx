9196c57480ed6e81e6a3022cb943fea7e4fbb090
Pratama89 <pratama.adiciptawan@gmail.com>
2023-11-04 16:35:11 +0700

    static

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -9,25 +9,30 @@ const ProductView = ({ products }: { products: ProductType[] }) => {
     return (
       <div className='w-full p-5 mb-5'> 
         <h1 className="text-2xl font-bold mt-5 text-center  ">Product</h1>
-          <div className="flex flex-wrap mx-auto lg:px-6 px-2 justify-evenly lg:justify-start mt-5 mb-5 border border-red-500 overflow-hidden"> 
-
-            <div className="w-">
-              <div className="" />
-              <div className="" />
-              <div className="" />
-              <div className="" />
-            </div>
-
-            {/* {products.map((product: ProductType) => (              
-              <div key={product.id} className="flex flex-col items-center rounded-lg  mb-5 lg:w-60 w-36 border border-blue-500 mx-2 ">
+          <div className="flex flex-wrap mx-auto lg:px-6 px-2 justify-evenly lg:justify-start md:justify-start mt-5 mb-5 border bg-slate-100 rounded-lg overflow-hidden"> 
+          {products.length > 0 ? (
+            <>
+              {products.map((product: ProductType) => (              
+              <div key={product.id} className="flex flex-col items-center rounded-lg mt-3 mb-3 lg:w-60 w-36 border bg-white  mx-2 ">
                 <img src={product.image} alt={product.name} className=" w-36 h-36 lg:w-60 lg:h-60 rounded-lg shadow-lg " />
-                  <div className="px-2 py-2 border border-red-500">
-                    <h4 className="font-bold text-xs md:text-sm lg:text-lg">{product.name}</h4>
-                    <p className="font-bold text-xs md:text-sm lg:text-lg">{product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</p>
-                    <p className="text-xs md:text-sm lg:text-lg">Stok: {product.stock}</p>                    
+                  <div className="px-2 py-2 ">
+                    <h4 className="font-bold text-xs md:text-xs lg:text-lg">{product.name}</h4>
+                    <p className="font-bold text-xs md:text-xs lg:text-lg">{product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</p>
+                    <p className="text-xs md:text-xs lg:text-lg">Stok: {product.stock}</p>                    
                   </div>                    
               </div>
-            ))} */}
+            ))}
+            </>
+          ) : (
+            <div className="w-1/4 p-3 animate-pulse">
+              <div className="w-full aspect-square rounded-sm bg-slate-400" />
+              <div className="w-full h-5 mt-1 bg-slate-400 rounded-sm" />
+              <div className="w-full h-4 mt-1 bg-slate-400 rounded-sm" />
+              <div className="w-full h-4 mt-2 bg-slate-400 rounded-sm" />
+            </div>            
+          )}
+
+            
           </div>
       </div>
 
