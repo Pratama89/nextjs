2cfecfe2a878d3f4a7e2ccea254b22b3baba30f5
Pratama89 <pratama.adiciptawan@gmail.com>
2023-11-09 12:22:28 +0700

    detail product

--- src/pages/detail_product/index.tsx
+++ src/pages/detail_product/index.tsx
@@ -3,6 +3,8 @@ import { ProductType } from "@/types/product.type"
 
 const DetailProduct = ({ product }: { product: ProductType }) => {
     return (
+        <>
+        <div className="text-2xl font-bold text-center">Detail Product</div>
         <div              
             className="flex flex-col items-center rounded-lg mt-3 mb-3 lg:w-60 w-36 border bg-white  mx-2 ">
             <img src={product.image} alt={product.name} className=" w-36 h-36 lg:w-60 lg:h-60 rounded-lg shadow-lg " />
@@ -12,6 +14,7 @@ const DetailProduct = ({ product }: { product: ProductType }) => {
                 <p className="text-xs md:text-xs lg:text-lg">Stok: {product.stock}</p>                    
             </div>                    
         </div>
+        </>
     )
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,25 +2,70 @@ import { fetcher } from "@/lib/swr/fetcher";
 import { useRouter } from "next/router"
 import useSWR from "swr";
 import DetailProduct from "../detail_product";
+import { ProductType } from "@/types/product.type";
 
-const DetailProductPage = () => {
+const DetailProductPage = ( {product}: {product: ProductType }) => {
     const { query } = useRouter()
 
-    const { data, error, isLoading } = useSWR(
-        `/api/product/${query.id}`,
-        fetcher
-      );
+    // const { data, error, isLoading } = useSWR(
+    //     `/api/product/${query.id}`,
+    //     fetcher
+    //   );
 
-    console.log(data);
+    // console.log(data);
 
     return (
         <div className='text-2xl font-bold text-center mt-5'>
-            <div>Detail Product</div>
-            <div className="flex justify-center">
-            <DetailProduct product={isLoading ? [] : data.data}/>
+            {/* clientsite */}
+            <div className="flex flex-col items-center">
+            {/* <DetailProduct product={isLoading ? [] : data.data}/> */}
+            {/* Server Site */}
+            <DetailProduct product={product} />
             </div>
         </div>
     )
 }
 
 export default DetailProductPage
+
+// dipanggil setiap melakukan request
+// export async function getServerSideProps({ 
+//     params, 
+// }: {
+//     params: {id: string}
+// }) {
+//     // console.log(params.product);
+//     // fetch data
+//     const res = await fetch(`http://localhost:3000/api/product/${params.id}`)
+//     const response = await res.json()
+//     console.log(response)
+
+//     return {
+//         props: {
+//             product: response.data
+//         }
+//     }
+// }
+
+export async function getStaticPaths() {
+    const res = await fetch('http://localhost:3000/api/product');
+    const response = await res.json();
+}
+
+export async function getStaticProps({ 
+    params, 
+}: {
+    params: {id: string}
+}) {
+    // console.log(params.product);
+    // fetch data
+    const res = await fetch(`http://localhost:3000/api/product/${params.id}`)
+    const response = await res.json()
+    // console.log(response)
+
+    return {
+        props: {
+            product: response.data
+        }
+    }
+}
